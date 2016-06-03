263f0d1740987e0378e774f299cc2ab3a8bf652a
wandalen <wandalen.me@gmail.com>
2016-06-03 11:12:33 +0300

    add files

---
+++ sample/simple1.js
@@ -0,0 +1,101 @@
+( function _Consequence_s_(){
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  require( 'wTools' );
+}
+
+//
+
+var _ = wTools;
+var Parent = function Alpha()
+{
+  this.methodOfAlpha = function(){ console.log( 'method of alpha' ); }
+}
+
+var Self = function Betta()
+{
+  if( !( this instanceof Self ) )
+  return new( _.routineJoin( Self, Self, arguments ) );
+  return Self.prototype.init.apply( this,arguments );
+}
+
+//
+
+var init = function init()
+{
+  var self = this;
+
+  _.mapExtendFiltering( _.filter.own(),self,Composes );
+  _.mapExtendFiltering( _.filter.own(),self,Aggregates );
+
+  if( options )
+  self.copy( options );
+
+}
+
+//
+
+var finit = function finit()
+{
+  var self = this;
+
+  Object.freeze( self );
+
+}
+
+//
+
+var methodOfBetta = function methodOfBetta()
+{
+  var self = this;
+
+  console.log( 'method of betta' );
+
+}
+
+//
+
+var Proto =
+{
+
+  init: init,
+  finit: finit,
+
+  methodOfBetta: methodOfBetta,
+
+  constructor: Self,
+  Composes: Composes,
+  Aggregates: Aggregates,
+  Restricts: Restricts,
+
+}
+
+var Static =
+{
+
+  staticMethod : staticMethod,
+
+}
+
+_.protoMake
+({
+  constructor : Self,
+  parent : Parent,
+  extend : Proto,
+  static : Static,
+});
+
+_global_.Alpha = Parent;
+_global_.Betta = Self;
+
+//
+
+var betta = new Betta();
+
+betta.methodOfAlpha();
+betta.methodOfBetta();
+
+})();
