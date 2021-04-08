26f70d798bee8260094a816d82024885677f7219
wandalen <wandalen.me@gmail.com>
2021-04-08 15:16:25 +0300

    .

--- proto/wtools/abase/l3_proto/Include.s
+++ proto/wtools/abase/l3_proto/Include.s
@@ -11,7 +11,7 @@
 if( typeof module !== 'undefined' )
 {
 
-  let _ = require( '../../../node_modules/Tools' );
+  const _ = require( '../../../node_modules/Tools' );
 
   _.include( 'wBlueprint' );
 
